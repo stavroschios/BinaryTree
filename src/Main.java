7aec7ec011a2a7f0900f7b919fa9ea0b55d5e1b9
Stavros <stavroschios@gmail.com>
2021-01-15 11:25:19 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.insert(1);
+        tree.insert(11);
+        tree.insert(12);
+        tree.insert(41);
+        tree.insert(51);
+
+
+
+    }
+}
